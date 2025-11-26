2826c95414d05b0d4adf34863a153d9eb5867edb
emyjnc <emily.junca@yahoo.fr>
2025-11-25 23:18:59 -0500

    Squashed commit of the following:
    commit 2aa049f36fee680cf97aa1da73984c1cfd13a263
    Author: emyjnc <emily.junca@yahoo.fr>
    Date:   Tue Nov 25 23:02:11 2025 -0500
    
        final
    
    commit e1d3634eddf0b6f8ecc8460c43eae68a8bab4e6c
    Author: emyjnc <emily.junca@yahoo.fr>
    Date:   Tue Nov 25 21:57:25 2025 -0500
    
        timer++
    
    commit eeee66afd7e952bdab61871eb892587b9f0542e7
    Author: emyjnc <emily.junca@yahoo.fr>
    Date:   Tue Nov 25 21:27:31 2025 -0500
    
        Timer
    
    commit 5f240ff1d7189494e6370269739ae3ec29d00cef
    Author: emyjnc <emily.junca@yahoo.fr>
    Date:   Tue Nov 25 19:51:56 2025 -0500
    
        envi + minijeu1 + scores
    
    commit 6293a7fa44cec3cd3d5077f9b51a80700aef14b1
    Author: emyjnc <emily.junca@yahoo.fr>
    Date:   Tue Nov 25 19:44:52 2025 -0500
    
        minijeu1 + envi + scores

---
+++ Assets/Scripts/TrashItem.cs
@@ -0,0 +1,51 @@
+using Oculus.Interaction;
+using UnityEngine;
+
+public class TrashItem : MonoBehaviour
+{
+
+    private Vector3 initPosition;
+    private Quaternion initRotation;
+    private bool hasStartedTimer = false;
+    private Grabbable grabbable;
+    private void Awake()
+    {
+        {
+            grabbable = GetComponent<Grabbable>();
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        initPosition = transform.position;
+        initRotation = transform.rotation;
+        GameManager.Instance.RegisterTrash();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStartedTimer)
+        {
+            if(Vector3.Distance(transform.position,initPosition) > 1f)
+            {
+                hasStartedTimer = true;
+                GameManager.Instance.startTimer();
+            }
+        }
+    }
+
+    public void ResetItem()
+    {
+        gameObject.SetActive(false);
+
+        transform.position = initPosition;
+        transform.rotation = initRotation;
+
+        gameObject.SetActive(true);
+    }
+
+   
+}
